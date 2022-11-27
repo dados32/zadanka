8b10ecd476714c7223559085303294f204b8aa45
Mateusz <mateuszdadas21@gmail.com>
2022-11-27 23:39:43 +0100

    poprawione z lokalką

--- największe z listy.py
+++ największe z listy.py
@@ -1,10 +1,13 @@
 def two_biggest_num(data):
-    biggest = max(data)
-    data.remove(biggest)
-    second_biggest = max(data)
-    return biggest, second_biggest
+
+    numbers = list(data)
+    biggest = max(numbers)
+    numbers.remove(biggest)
+    second_biggest = max(numbers)
+    return print(biggest, second_biggest)
 
 
 if __name__ == '__main__':
-    numbers = [2, 1, 4, 6, 88, 0, 5, 66, 4534, 999, 0, 2, -49]
-    two_biggest_num(numbers)
+    data = [2, 1, 4, 6, 88, 0, 5, 66, 4534, 999, 0, 2, -49]
+    two_biggest_num(data)
+    print(data)
