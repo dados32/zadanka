5344423e23b9e534949aca26bcb8484a287ab657
dados32 <83305546+dados32@users.noreply.github.com>
2022-11-27 23:41:27 +0100

    Update największe z listy.py
    usunięte printy

--- największe z listy.py
+++ największe z listy.py
@@ -4,10 +4,9 @@ def two_biggest_num(data):
     biggest = max(numbers)
     numbers.remove(biggest)
     second_biggest = max(numbers)
-    return print(biggest, second_biggest)
+    return biggest, second_biggest
 
 
 if __name__ == '__main__':
     data = [2, 1, 4, 6, 88, 0, 5, 66, 4534, 999, 0, 2, -49]
     two_biggest_num(data)
-    print(data)
